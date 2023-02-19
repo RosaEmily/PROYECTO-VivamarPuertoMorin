4f4bfa912fb182387d55a37cfb7d7dce94859926
RosaERH <rosa.rodriguez@janaq.com>
2023-02-19 00:42:30 -0500

    Fix

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="../assets/img/Logo.PNG" alt="" style="width: 200px;"></a>
+            <a class="navbar-brand" href="#">VIVAMAR</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
