461b123e37a2c3c00f73ee5ca2d6a5f6d3d10b41
RosaERH <rosa.rodriguez@janaq.com>
2023-02-19 01:12:56 -0500

    Fix is solved?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default {
   nitro: {
     preset: 'vercel-edge',
   },
+  alias: {
+    assets: '/<rootDir>/assets',
+  },
   css: [
     // CSS file in the project
     '@/assets/css/main.css',
