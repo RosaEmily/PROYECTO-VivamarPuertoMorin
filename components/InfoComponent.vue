787c7031a390e3c46fd1c1b13b57377d6bcfe3fd
RosaERH <rosa.rodriguez@janaq.com>
2023-02-28 21:30:09 -0500

    Info section

---
+++ components/InfoComponent.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <h4 class="secondary-title text-center">¿Por qué elegirnos?</h4>
+        <br>
+        <div class="row justify-content-evenly">
+            <div class="col-md-5 text-center mb-3">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 col-md-12" style=" min-height: 200px; background-image: url(https://s.ineventos.com/cr/2018/09/124461/casa-de-playa-eventos-celamar-277278-i-640w.jpg); background-position: center; background-size: cover;">
+                        </div>
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">Disponibilidad</h5>
+                                <p class="card-text">de alquiler para alojamiento vacacional con familia y/o amigos así como para la realización de eventos previa coordinación.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 text-center mb-3">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 col-md-12" style=" min-height: 200px; background-image: url(https://scontent.ftru2-1.fna.fbcdn.net/v/t39.30808-6/330990999_1334497073778591_8262197994156431727_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=9HuNs6xFraMAX8fu7wI&_nc_ht=scontent.ftru2-1.fna&oh=00_AfDcnNqbe9EbcFWbf3CSr54YEwwlwt4-gWTYsgJ3kd054g&oe=64034B02); background-position: center; background-size: cover;">
+                        </div>
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">Ubicación</h5>
+                                <p class="card-text">de fácil acceso y a pocas horas del centro de Trujillo, Vivamar cuenta con salida directa a la playa Puerto Morin.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-none d-xl-block col-md-5 text-center mb-3">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">Descuentos</h5>
+                                <p class="card-text">por temporada según cantidad de días de estadía y personas en grupo, así como diversos métodos de pago para brindar facilidades al cliente.</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-12" style=" min-height: 200px; background-image: url(https://img.computeruniverse.net/content/aktionen/SummerSALE/bannerset-575x300-2022-kw30-summersale-en-nocta.jpg); background-position: center; background-size: cover;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-none d-xl-block col-md-5 text-center mb-3">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">Equipamiento</h5>
+                                <p class="card-text">en dormitorios, sala y cocina para hacer de tu estancia la mejor posible, Vivamar también cuenta con amplias áreas verdes para realizar camping.</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-12" style=" min-height: 200px; background-image: url(https://scontent.ftru2-1.fna.fbcdn.net/v/t39.30808-6/331216571_530428149230420_7084250616269301712_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=HiywGHf_87MAX-Uxym9&_nc_ht=scontent.ftru2-1.fna&oh=00_AfBNdVAcJErYz8ksvZE5aF-w8rAUqBcVeFO7fQ-pcjPwNA&oe=64033E13); background-position: center; background-size: cover;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-xl-none col-md-5 text-center mb-3">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 col-md-12" style=" min-height: 200px; background-image: url(https://img.computeruniverse.net/content/aktionen/SummerSALE/bannerset-575x300-2022-kw30-summersale-en-nocta.jpg); background-position: center; background-size: cover;">
+                        </div>
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">Descuentos</h5>
+                                <p class="card-text">por temporada según cantidad de días de estadía y personas en grupo, así como diversos métodos de pago para brindar facilidades al cliente.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-xl-none col-md-5 text-center mb-3">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-xl-6 col-md-12" style=" min-height: 200px; background-image: url(https://scontent.ftru2-1.fna.fbcdn.net/v/t39.30808-6/331216571_530428149230420_7084250616269301712_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=HiywGHf_87MAX-Uxym9&_nc_ht=scontent.ftru2-1.fna&oh=00_AfBNdVAcJErYz8ksvZE5aF-w8rAUqBcVeFO7fQ-pcjPwNA&oe=64033E13); background-position: center; background-size: cover;">
+                        </div>
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">Equipamiento</h5>
+                                <p class="card-text">en dormitorios, sala y cocina para hacer de tu estancia la mejor posible, Vivamar también cuenta con amplias áreas verdes para realizar camping.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,11 @@
         <div class="container-fluid p-0">
             <CarrouselComponent></CarrouselComponent>
         </div>
-        <div class="container" style="margin-top: -100px">
+        <div class="container" style="margin-top: -80px">
             <DetailsComponent></DetailsComponent>
         </div>
-        <div class="container" style="margin-top: 30px">
-            A
+        <div class="container" style="margin-top: 50px">
+            <InfoComponent></InfoComponent>
         </div>
     </div>
 </template>
@@ -17,11 +17,13 @@
     import HeaderComponent from '../components/HeaderComponent.vue';
     import CarrouselComponent from '../components/CarrouselComponent.vue';
     import DetailsComponent from '../components/DetailsComponent.vue';
+    import InfoComponent from '../components/InfoComponent.vue';
     export default {
         components: { 
             HeaderComponent,
             CarrouselComponent,
             DetailsComponent,
+            InfoComponent,
         },
         layout: 'main'
     }
